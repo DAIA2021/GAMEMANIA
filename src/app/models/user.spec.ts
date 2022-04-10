4300bd0ff5b9abfb8b217c7e5113aa8a2054d765
Daiana Oliveira <daiana.davi2411@gmail.com>
2022-04-10 17:29:55 -0300

    UC10 Encontro Remto2 Logins testes tela com erro

---
+++ src/app/models/user.spec.ts
@@ -0,0 +1,11 @@
+import { User } from './user';
+
+describe('User', () => {
+  it('should create an instance', () => {
+    expect(newUser()).toBeTruthy();
+  });
+});
+function newUser(): any {
+  throw new Error('Function not implemented.');
+}
+
